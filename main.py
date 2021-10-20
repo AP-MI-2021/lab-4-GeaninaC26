e49f4b7f830c238de5e8719c366e3aeeeb33d71b
Geanina <gianinaclipa@hmail.com>
2021-10-20 10:57:38 +0300

    Partial

---
+++ main.py
@@ -0,0 +1,26 @@
+def show_menu():
+    print("1.")
+    print("2.")
+    print("3.")
+    print("4.")
+
+
+def main():
+    lst = []
+    while True:
+        show_menu()
+        option = input("Alegeti o optiune: ")
+        if option == '1':
+            pass
+        elif option == '2':
+            pass
+        elif option == '3':
+            pass
+        elif option == '4':
+            pass
+        elif option == '5':
+            pass
+        elif option == 'x':
+            break
+        else:
+            print("Optiune nevalida")
